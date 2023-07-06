47a29143530c3b43d8e324b6970d1e401e08eb72
ludmiviale <ludmilaviale@hotmail.com>
2023-07-06 14:46:13 -0300

    ajustando ejercicios

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -52,8 +52,7 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
-   var num = objetoMisterioso.numeroMisterioso;
-   var resultado = num*5
+   var resultado = objetoMisterioso.numeroMisterioso*5
    return resultado;
 }
 
